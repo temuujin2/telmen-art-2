9d3442d497f5c3fae7ad6e44bcdbf24799765f81
Temuujin Erdenebileg <102133245+temuujin2@users.noreply.github.com>
2022-11-12 22:21:22 +0800

    update add news page

---
+++ src/component/BlogCard.js
@@ -0,0 +1,89 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { grey } from '@mui/material/colors';
+import ShareIcon from '@mui/icons-material/Share';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import back1 from '../images/back-1.jpeg'
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
+export default function RecipeReviewCard() {
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Card sx={{ maxWidth: 500 }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: grey[900] }} aria-label="recipe">
+            T
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title="The Legend"
+        subheader="November 10, 2022"
+      />
+      <CardMedia
+        component="img"
+        height="194"
+        image={back1}
+        alt="Paella dish"
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          This impressive paella is a perfect party dish and a fun meal to cook
+          together with your guests. Add 1 cup of frozen peas along with the mussels,
+          if you like.
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>Method:</Typography>
+          <Typography paragraph>
+            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
+            aside for 10 minutes.
+          </Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
+}
--- src/pages/News.js
+++ src/pages/News.js
@@ -1,6 +1,10 @@
 import imgBack_about from '../images/BGGGG.png'
 import React from 'react'
-
+import RecipeReviewCard from '../component/BlogCard'
+import RecipeReviewCard2 from '../component/BlogCard2'
+import RecipeReviewCard3 from '../component/BlogCard3'
+import RecipeReviewCard4 from '../component/BlogCard4'
+import { Box } from '@mui/system'
 
 
 export const News = () => {
@@ -8,6 +12,21 @@ export const News = () => {
         <section>
             <div className="news-wrapp">
                 <h2>NEWS</h2>
+                <Box mt={10} display="flex" gap={10} sx={{justifyContent:'center', flexWrap:'wrap', width:'80%'}}>
+                    <Box>
+                        <RecipeReviewCard />
+                    </Box>
+                    <Box>
+                        <RecipeReviewCard2 />
+                    </Box>
+                    <Box>
+                        <RecipeReviewCard3 />
+                    </Box>
+                    <Box>
+                        <RecipeReviewCard4 />
+                    </Box>
+                </Box>
+
                 
             </div>
 
